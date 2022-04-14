7aa56cb4448376a69db4a2db573c33b81057d868
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-14 23:23:44 +0200

    Add ex 07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,10 +1,11 @@
 
 let numberArray = new Set([9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5]);
 let uniqueArray = [];
-;
+
 for ( let set of numberArray){
     uniqueArray.push(set);
 }
-uniqueArray.sort();
+//ASC
+uniqueArray.sort((a,b)=>a-b);
 console.log(uniqueArray);
 module.exports = {numberArray, uniqueArray};
