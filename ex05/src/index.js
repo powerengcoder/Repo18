eafe28e4c566ec293207ecb247a6236f0a5bf8bd
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-14 22:05:43 +0200

    Add ex 05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,11 +1,23 @@
-let shoppingList = new Map();
+let shoppingList = new Map([
+  ['Banana', 3],
+  ['Pineapple', 5],
+  ['Pear', 2],
+  ['Carrot', 10],
+  ['Apple', 1.5],
+]);
 
-shoppingList.set('groceries','Banana');
-shoppingList.set('groceries','Pineapple'); 
-shoppingList.set('groceries','Carrot');
-shoppingList.set('groceries','Apple');
+var arrKey = [];
+for(let x of shoppingList.entries()) {
+    arrKey.push(x[0]);
+    console.log(`groceries: ${x[0]}`);
 
+}
 
-for(let value of shoppingList) console.log(value);
+var arrValue = [];
+for(let x of shoppingList.entries()) {
+    arrKey.push(x[1]);
+    console.log(`amount: ${x[1]}`);
 
+}
 
+for (let value of shoppingList) console.log(value);
