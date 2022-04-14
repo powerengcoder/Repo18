dbe7fd8bab613643c807f804c0d22ea62188462b
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-14 23:18:52 +0200

    Add ex 06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,11 +1,17 @@
 let rockStar = new Map([
-[artist, 'The Rolling Stones']
-[song, 'Angie']
-[album, 'Goats Head Soup']
-[singer, 'Mick Jagger']
+['artist', 'The Rolling Stones'],
+['song', 'Angie'],
+['album', 'Goats Head Soup'],
+['singer', 'Mick Jagger']
 ]);
 
 
 let newRock = new Map();
+
+for(let x of rockStar.entries()) {
+    if(x[0].startsWith("a") === false)
+    newRock.set(x[0], x[1]);
+}
+console.log(newRock);
 
 module.exports = {rockStar, newRock};
